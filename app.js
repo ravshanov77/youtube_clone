9da71e6edd41cc0e4c914aa8c122ec4b694dcab9
Farrukh <farruxlynx@gmail.com>
2024-05-26 21:22:31 +0500

    Initial commit

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const noLinkButton = document.querySelector(".three-dots-icon");
-
-    noLinkButton.addEventListener("click", function (event) {
-      event.preventDefault();
-      event.stopPropagation();
-    });
-  });
